ffbe3aab15e5ad3d4fab5392fd3cf7688bd6efc6
Jeetendra <jeetendradakua@gmail.com>
2022-04-21 12:23:41 +0530

    upadted code for ngrok test

--- src/main/java/com/qa/opencart/factory/DriverFactory.java
+++ src/main/java/com/qa/opencart/factory/DriverFactory.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Properties;
 
 import org.apache.commons.io.FileUtils;
@@ -13,6 +15,7 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
 import com.qa.opencart.utils.Browser;
@@ -46,15 +49,29 @@ public class DriverFactory {
 
 		if (browserName.equalsIgnoreCase(Browser.CHROME_BROWSER_VALUE)) {
         log.info("running test on browser:"+browserName);
+      
+        if (Boolean.parseBoolean(prop.getProperty("remote"))) {
+			init_remoteWebDriver(Browser.CHROME_BROWSER_VALUE);
+		} else {
+			
+		// local execution :
 			WebDriverManager.chromedriver().setup();
 			// System.getProperty(Browser.CHROME_DRIVER_BINARY_KEY,Browser.CHROME_DRIVER_PATH);
 			// driver = new ChromeDriver(optionsManager.getChromeOptions());
 			tlDriver.set(new ChromeDriver(optionsManager.getChromeOptions()));
+		}
 		} else if (browserName.equalsIgnoreCase(Browser.FIREFOX_BROWSER_VALUE)) {
+			
+			if (Boolean.parseBoolean(prop.getProperty("remote"))) {
+				init_remoteWebDriver(Browser.FIREFOX_BROWSER_VALUE);
+			} else {
+			
+			
 			WebDriverManager.firefoxdriver().setup();
 			// System.getProperty(Browser.FIREFOX_DRIVER_BINARY_KEY,Browser.FIREFOX_DRIVER_PATH);
 			// driver = new FirefoxDriver(optionsManager.getFirefoxOptions());
 			tlDriver.set(new FirefoxDriver(optionsManager.getFirefoxOptions()));
+			}
 		} else if (browserName.equalsIgnoreCase(Browser.SAFARI_BROWSER_VALUE)) {
 			// driver = new SafariDriver();
 			tlDriver.set(new SafariDriver());
@@ -67,6 +84,25 @@ public class DriverFactory {
 		getDriver().get(prop.getProperty("url"));
  
 		return getDriver();
+	}
+
+	private void init_remoteWebDriver(String browserName ) {
+		System.out.println("Running test cases on remote grid server :"+ browserName);
+		if (browserName.equalsIgnoreCase("chrome")) {
+			try {
+				tlDriver.set(
+						new RemoteWebDriver(new URL(prop.getProperty("huburl")), optionsManager.getChromeOptions()));
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+			}
+		} else if (browserName.equalsIgnoreCase("firefox")) {
+			try {
+				tlDriver.set(
+						new RemoteWebDriver(new URL(prop.getProperty("huburl")), optionsManager.getFirefoxOptions()));
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+			}	
+		}
 	}
 
 	/**
--- src/test/java/com/qa/opencart/base/BaseTest.java
+++ src/test/java/com/qa/opencart/base/BaseTest.java
@@ -5,6 +5,7 @@ import java.util.Properties;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 import org.testng.asserts.SoftAssert;
 
 import com.qa.opencart.factory.DriverFactory;
@@ -26,10 +27,17 @@ public class BaseTest {
 	public RegistrationPage registrationPage;
 	public SoftAssert softAssert;
 	
+	
+	@Parameters({"browser"})
 	@BeforeTest
-	public void setup() {
+	public void setup(String browser) {
 		df = new DriverFactory();
 		prop = df.init_prop();
+		
+		if(browser != null) {
+			prop.setProperty("browser", browser);
+		}
+		
 		driver = df.init_driver(prop);
 		loginPage = new LoginPage(driver);
 		softAssert = new SoftAssert();
